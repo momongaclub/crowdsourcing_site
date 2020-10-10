9587a75bf44493eef7f615e087988354769dcc23
momongaclub <momongaclub8@gmail.com>
2020-10-10 11:27:51 +0900

    backup

--- flask/crowd_site.py
+++ flask/crowd_site.py
@@ -1,21 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 import pandas as pd
 import sqlite3
+
 import random
 import string
-import datetime
-import time
 import json
+import time
 
-app = Flask(__name__)
+# app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static')
 
 
 @app.route('/')
-def hello():
-    return render_template('hello.html', title='crowdsorcing_site')
+def home():
+    return render_template('index.html', title='crowdsorcing_site')
 
-@app.route('/input_form', methods = ['POST', 'GET'])
-def confirm():
+
+@app.route('/input_form', methods=['POST', 'GET'])
+def input_form():
     if request.method == 'POST':
         try:
             dbname = "./database/rap.db"
@@ -32,15 +36,21 @@ def confirm():
             cur.execute('SELECT tid FROM reserve WHERE wid = ?', (wid, ))
             tid = cur.fetchone()
 
-            if tid == None: # wid is not in the reserve table
+            if tid is None:
+                # wid is not in the reserve table
                 df = pd.read_sql('SELECT task.tid, \
-                                  CASE WHEN agg_w.c IS NULL THEN 0 ELSE agg_w.c END AS wc, \
-                                  CASE WHEN agg_r.c IS NULL THEN 0 ELSE agg_r.c END AS rc \
+                                  CASE WHEN agg_w.c IS NULL THEN 0 \
+                                  ELSE agg_w.c END AS wc, \
+                                  CASE WHEN agg_r.c IS NULL THEN 0 \
+                                  ELSE agg_r.c END AS rc \
                                   FROM task LEFT JOIN \
-                                 (SELECT tid, COUNT(*) c FROM work GROUP BY tid) agg_w \
+                                 (SELECT tid, COUNT(*) c FROM work \
+                                  GROUP BY tid) agg_w \
                                   ON task.tid = agg_w.tid LEFT JOIN \
-                                 (SELECT tid, COUNT(*) c FROM reserve GROUP BY tid) agg_r \
-                                  ON task.tid = agg_r.tid ORDER BY wc + rc LIMIT 1', conn)
+                                 (SELECT tid, COUNT(*) c FROM reserve \
+                                  GROUP BY tid) agg_r \
+                                  ON task.tid = agg_r.tid ORDER BY wc + rc \
+                                  LIMIT 1', conn)
                 tid = df['tid'][0]
                 cur.execute('INSERT INTO reserve VALUES(?, ?, ?)', (wid, tid, unix_time))
             else:
@@ -52,7 +62,6 @@ def confirm():
             task_info = cur.fetchone()
             task_info = task_info[0]
             task_info_json = json.loads(task_info)
-
         except Exception as e:
             conn.rollback()
             raise e
@@ -60,12 +69,10 @@ def confirm():
             conn.commit()
             cur.close()
             conn.close()
-        # return render_template("confirm.html", result=result)
-        # return render_template("confirm.html", tid=tid, wid=wid)
-        # return render_template("confirm.html", result=result, url=task_info_json['s_url'], s_time=task_info_json['s_t'], e_time=task_info_json['e_t'])
-        return render_template("confirm.html", result=result, url=task_info_json['s_url'], s_time=task_info_json['s_t'], e_time=task_info_json['e_t'], worker_id=wid, task_id=tid)
+        return render_template("input_form.html", result=result, url=task_info_json['s_url'], s_time=task_info_json['s_t'], e_time=task_info_json['e_t'], worker_id=wid, task_id=tid)
 
-@app.route('/result', methods = ['POST', 'GET'])
+
+@app.route('/result', methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
         result = request.form
@@ -73,12 +80,16 @@ def result():
         dbname = "./database/rap.db"
         conn = sqlite3.connect(dbname)
         cur = conn.cursor()
-        cur.execute("INSERT INTO work VALUES (?, ?, ?, ?, ?)", data)
+        # taskidとwidからreserveのr_timeを取得
+        cur.execute('SELECT r_time FROM reserve WHERE tid = ? AND wid = ?', (result['task_id'], result['worker_id']))
+        r_time = cur.fetchone()[0]
+        task_time = int(time.time() - r_time)
+        cur.execute('INSERT INTO work VALUES (?, ?, ?, ?, ?)', data)
         conn.commit()
         cur.close()
         conn.close()
         random_id = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
-        rancers_key = 'iM'+random_id+'aB'
+        rancers_key = str(task_time)+'iM'+random_id+'aB'
         return render_template("result.html", result=result, url=result['url'], rancers_key=rancers_key)
 
 
